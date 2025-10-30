60b5edbb6f415b6b29cd54c1f66048dda630607f
benjamin kähr <benjkaehr1@gmail.com>
2025-10-30 20:46:30 +0100

    Game Screen erstellt
    Added "Screens"-Section at the Start
    
    Added:
     * Angreifen - Button
     * Verteidigen - Button
     * Inventar - Button
    
    Changed Lines:
     * 17-24
     * 58
     * 67-79

--- Py-RPG-game/Py_RPG_game.py
+++ Py-RPG-game/Py_RPG_game.py
@@ -14,6 +14,14 @@ root.bind_all("<Escape>",on_escape)
 root.resizable(False,False)
 
 
+#--Screens--
+Game_Screen = tk.Frame(root, bg="#FFFFFF")
+def show_game():
+    Menu_Screen.forget()
+    Game_Screen.tkraise()
+    Game_Screen.pack(fill="both", expand=True)
+    Game_Normal_Buttons()
+
 #--Grundvarabeln Variablen--
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
@@ -47,7 +55,7 @@ canvas.create_line(100, screen_height - 100, screen_width - 100, screen_height -
 Menu_title = tk.Label(Menu_Screen, text="Py-RPG-game", font=("Arial", 48, BOLD), fg="#000000", bg="#FFFFF5" )
 Menu_title.place(relx=0.5, rely=0.1, anchor="center")
 
-Menu_Start_Button = tk.Button(Menu_Screen, text="Start Game", font=("Arial", 28), fg="#000000", bg="#FFFFC2", width=10, height=1)
+Menu_Start_Button = tk.Button(Menu_Screen, text="Start Game", font=("Arial", 28), fg="#000000", bg="#FFFFC2", width=10, height=1, command=show_game)
 Menu_Start_Button.place(relx=0.5, rely=0.375, anchor="center")
 
 Menu_Settings_Button = tk.Button(Menu_Screen, text="Settings", font=("Arial", 28), fg="#000000", bg="#FFFFC2", width=10, height=1)
@@ -55,6 +63,19 @@ Menu_Settings_Button.place(relx=0.5, rely=0.45, anchor="center")
 
 Menu_Screen_Button = tk.Button(Menu_Screen, text="Exit Game", font=("Arial", 28), fg="#000000", bg="#FFC2C2", width=10, height=1, command=root.destroy)
 Menu_Screen_Button.place(relx=0.5, rely=0.525, anchor="center")
+
+
+
+#---Game---
+def Game_Normal_Buttons():
+    Game_Angrerifen_Button = tk.Button(Game_Screen, text="Angreifen", font=("Arial", 28), fg="#000000", bg="#FFFFC2", width=10, height=1)
+    Game_Angrerifen_Button.place(relx=0.74, rely=0.8, anchor="center")
+    Game_Verteidigen_Button = tk.Button(Game_Screen, text="Verteidigen", font=("Arial", 28), fg="#000000", bg="#FFFFC2", width=10, height=1)
+    Game_Verteidigen_Button.place(relx=0.8, rely=0.73, anchor="center")
+    Game_Inventar_Button = tk.Button(Game_Screen, text="Inventar", font=("Arial", 28), fg="#000000", bg="#FFFFC2", width=10, height=1)
+    Game_Inventar_Button.place(relx=0.86, rely=0.66, anchor="center")
+
+
 
 #--Main loop needs to be at the end of the file--
 root.mainloop()
